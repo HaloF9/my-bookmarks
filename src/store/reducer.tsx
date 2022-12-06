88c7e8439a7640695fc373e45b43d7b800f3474a
Alois Dougere <alois.dougere.externe@capgemini.com>
2022-12-06 18:04:58 +0100

    update

--- src/store/reducer.tsx
+++ src/store/reducer.tsx
@@ -1,52 +1,50 @@
-import { ADD_BOOKMARK, EDIT_BOOKMARK, REMOVE_BOOKMARK } from './actionTypes';
-
+import { ADD_BOOKMARK, EDIT_BOOKMARK, REMOVE_BOOKMARK } from "./actionTypes";
 
 const initialState: BookmarkState = {
-  bookmarks: []
-}
+  bookmarks: [],
+};
 
 const reducer = (
   state: BookmarkState = initialState,
   action: BookmarkAction
 ): BookmarkState => {
-  const newBookmark = action.bookmark
-  let updatedBookmarks: IBookmark[]
+  const newBookmark = action.bookmark;
+  let updatedBookmarks: IBookmark[];
   switch (action.type) {
     case ADD_BOOKMARK:
-
       // Do not add an existing bookmark (based on randomly generated id's : not the best way)
-      const idAlreadyExist = state.bookmarks.find((bookmark) => bookmark.id === newBookmark.id)
+      const idAlreadyExist = state.bookmarks.find(
+        (bookmark) => bookmark.id === newBookmark.id
+      );
 
-      if (newBookmark && !idAlreadyExist) return {
-        ...state,
-        bookmarks: state.bookmarks.concat(newBookmark),
-      }
+      if (newBookmark && !idAlreadyExist)
+        return {
+          ...state,
+          bookmarks: [newBookmark, ...state.bookmarks],
+        };
 
       // Return state if no newest bookmark created
-      return state
+      return state;
 
     case EDIT_BOOKMARK:
-
       // Update Bookmark list
-      updatedBookmarks = state.bookmarks.map(
-        (bookmark) => {
-          if (bookmark.id === newBookmark.id) return newBookmark
-          return bookmark
-        }
-      )
+      updatedBookmarks = state.bookmarks.map((bookmark) => {
+        if (bookmark.id === newBookmark.id) return newBookmark;
+        return bookmark;
+      });
       return {
         ...state,
         bookmarks: updatedBookmarks,
-      }
+      };
 
     case REMOVE_BOOKMARK:
       updatedBookmarks = state.bookmarks.filter(
         (bookmark) => bookmark.id !== action.bookmark.id
-      )
+      );
       return {
         ...state,
         bookmarks: updatedBookmarks,
-      }
+      };
   }
   return state;
 };
